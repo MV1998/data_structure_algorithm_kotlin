24f5d81320ca2d8a309824137f0a0fa410af7b51
Mohit Varma <mv501049@gmail.com>
2023-12-20 09:32:51 +0530

    deletion in LL.

--- src/linked_list/Runner.kt
+++ src/linked_list/Runner.kt
@@ -1,21 +1,41 @@
 package linked_list
 
 fun main() {
+
+    // Tip while solving linked list question
+    // start thinking edge case
+    // like head is null, head.next is null
+
+
+
     // Lecture - 1 : take u forward
     // Create Linked list
     // Array to linked list
     // traversal
     // search an element
     // find length of linked list
-    val arr = intArrayOf(1,2,3,4,5,6)
-    val head = arrToLinkedList(arr)
+    val arr = intArrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13)
+    var head : MLinkedList? = arrToLinkedList(arr)
     traversalLinkedList(head)
     println("Length of Linked Link ${lengthOfLinkedList(head)}")
     println("Search result ${searchAnElementInLinkedList(head, 5)}")
 
 
-    // Lecture - 2 - In progress
+    // Lecture - 2 - Deletion in LL
+    // Delete head
+    // Delete tail
+//    head = removedHead(head)
+//    println("after removing head ")
+//    traversalLinkedList(head)
+//    println()
+//
+//    head = removedTail(head)
+//    println("after removing tail ")
+//    traversalLinkedList(head)
 
+    head = removeKthNode(head, 2)
+    println("deleting with kth node")
+    traversalLinkedList(head)
 }
 
 // Time complexity - o(n)
@@ -31,7 +51,7 @@ fun arrToLinkedList(arr : IntArray) : MLinkedList {
 }
 
 // Time complexity - o(n)
-fun traversalLinkedList(head : MLinkedList) {
+fun traversalLinkedList(head : MLinkedList?) {
     var temp : MLinkedList? = head
     while (temp != null) {
         print("${temp.data} ")
@@ -60,4 +80,47 @@ fun searchAnElementInLinkedList(head : MLinkedList?, search : Int) : Boolean {
         temp = temp.next
     }
     return false
+}
+
+fun removedHead(head : MLinkedList?) : MLinkedList? {
+    var newHead = head
+    if (head == null) return null
+    var temp = newHead
+    newHead = newHead?.next
+    return newHead
+}
+
+fun removedTail(head : MLinkedList?) : MLinkedList? {
+    var newHead = head
+    if (newHead == null || newHead.next == null) return null
+    var temp = newHead
+    while (temp?.next?.next != null) {
+        temp = temp.next
+    }
+    temp?.next = null
+    return newHead
+}
+
+fun removeKthNode(head : MLinkedList?, k : Int) : MLinkedList? {
+    var newHead = head
+    if (newHead == null) return null
+    if (k == 1) {
+        var temp = newHead
+        newHead = newHead.next
+        return newHead
+    }
+
+    var nk = 1
+    var temp = newHead
+    while (temp?.next != null) {
+        if (nk + 1 == k) {
+            temp.next = temp.next?.next
+            break
+        }else {
+            temp = temp.next
+            ++nk
+        }
+    }
+
+    return newHead
 }
