6377b63821ffced7445fe884e7255896fc6fd81b
Mohit Varma <mv501049@gmail.com>
2023-12-22 23:38:52 +0530

    new node added at the tail of the linked list.

--- src/linked_list/DLLRunner.kt
+++ src/linked_list/DLLRunner.kt
@@ -2,18 +2,47 @@ package linked_list
 
 fun main() {
     var head = addAtHeadDLL(null, 1)
-     head = addAtHeadDLL(null, 2)
-     head = addAtHeadDLL(null, 3)
-     head = addAtHeadDLL(null, 4)
+     head = addAtHeadDLL(head, 2)
+     head = addAtHeadDLL(head, 3)
+     head = addAtHeadDLL(head, 4)
 
-    println(head?.data)
+    traversalOfDLL(head)
+    println()
+    addAtTailOfDLL(head, 10)
+    traversalOfDLL(head)
+    println()
+    addAtTailOfDLL(head, 12)
+    traversalOfDLL(head)
+    println()
 }
 
-fun addAtHeadDLL(head : MDLinkedList?, data : Int) : MDLinkedList?  {
+fun addAtHeadDLL(head : MDLinkedList?, data : Int) : MDLinkedList  {
     if (head == null) return MDLinkedList(data)
 
     var temp = MDLinkedList(data)
     temp.next = head
     head.prev = temp
     return temp
+}
+
+fun addAtTailOfDLL(head : MDLinkedList?, data : Int) : MDLinkedList {
+    if (head == null) return MDLinkedList(data)
+
+    var temp = head
+    while (temp?.next != null) {
+        temp = temp.next
+    }
+    var newNode = MDLinkedList(data)
+    temp?.next = newNode
+    newNode.prev = temp
+
+    return head
+}
+
+fun traversalOfDLL(head: MDLinkedList?) {
+    var temp = head
+    while (temp != null) {
+        print("${temp.data} ")
+        temp = temp.next
+    }
 }
