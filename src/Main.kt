fd58f39dcbf3ea4351456cdceb8e2cc1ee62b671
Mohit Varma <mv501049@gmail.com>
2024-01-09 00:04:20 +0530

    merge sort done

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,3 @@
-import sorting.SelectionSort
-
 fun main() {
 
 }
--- src/sorting/Runner.kt
+++ src/sorting/Runner.kt
@@ -1,11 +1,17 @@
 package sorting
 
 fun main() {
-    val selectionSort = SelectionSort()
-    var list = intArrayOf(3,1)
-    selectionSort.sort(list)
-    for (element in list) {
-        print("$element ")
+    val selectionSort = Sort()
+    var list = intArrayOf(1, 0, 3, 4, 2)
+//    selectionSort.selectionSort(list)
+//    for (element in list) {
+//        print("$element ")
+//    }
+    var arrToBeSorted = intArrayOf(1, 0, 3, 4, 2)
+//    selectionSort.mergeSort(arrToBeSorted, 0, arrToBeSorted.size-1)
+//    arrToBeSorted.sortDescending()
+    for (i in arrToBeSorted) {
+        print("$i ")
     }
     println()
 }
