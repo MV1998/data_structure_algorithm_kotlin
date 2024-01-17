961f8986144210727ea9595034cbc1901b656b9e
Mohit Varma <mv501049@gmail.com>
2024-01-17 23:53:24 +0530

    remove outer parantheses

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,29 @@
-fun main() {
+import java.util.*
 
+fun main() {
+    print(removeOuterParentheses("(()())(())(()(()))"))
+}
+
+fun removeOuterParentheses(s: String): String {
+    var stack : Stack<Char> = Stack()
+    var string : StringBuilder = StringBuilder()
+    for (ch in s) {
+        if (stack.isEmpty()) {
+            stack.push(ch)
+            continue
+        }
+        if (ch == '(' && stack.peek() == ')' && !stack.isEmpty()) {
+            stack.pop()
+            string.append(ch)
+            continue
+        }
+        if (ch == ')' && stack.peek() == '(' && !stack.isEmpty()) {
+            stack.pop()
+            string.append(ch)
+            continue
+        }
+        stack.push(ch)
+        string.append(ch)
+    }
+    return string.toString()
 }
