3e890a2c43a4c2dafdf4403d8bd145c3c8d36c45
Mohit Varma <mv501049@gmail.com>
2024-01-25 23:58:34 +0530

    understanding flatten() and flatMap() method of the collection.

--- src/kotlin_collection_practice/CollectionRunner.kt
+++ src/kotlin_collection_practice/CollectionRunner.kt
@@ -14,7 +14,24 @@ fun main() {
         Book(name = "Math7", 70.3, "Math", 234, Date.from(Instant.now())),
     )
 
-    var groupedByPrice = books.groupBy { book -> book.price }
-    groupedByPrice.forEach { (t, u) -> println("$t has ${u.size}") }
-    
+//    var groupedByPrice = books.groupBy { book -> book.price }
+//    groupedByPrice.forEach { (t, u) -> println("$t has ${u.size}") }
+
+    var listOfListOfBooks : List<List<Book>> = listOf(listOf(Book(name = "Math1", 120.3, "Math", 234, Date.from(Instant.now())),
+        Book(name = "Math2", 320.3, "Math", 234, Date.from(Instant.now())),
+        Book(name = "Math3", 520.3, "Math", 234, Date.from(Instant.now())),
+        Book(name = "Math4", 720.3, "Math", 234, Date.from(Instant.now())),
+        Book(name = "Math5", 220.3, "Math", 234, Date.from(Instant.now())),
+        Book(name = "Math16", 20.3, "Math", 234, Date.from(Instant.now())),
+        Book(name = "Math7", 70.3, "Math", 234, Date.from(Instant.now())),),
+
+        listOf(Book(name = "Math1", 120.3, "Math", 234, Date.from(Instant.now())),
+            Book(name = "Math2", 320.3, "Math", 234, Date.from(Instant.now())),
+            Book(name = "Math3", 520.3, "Math", 234, Date.from(Instant.now())),
+            Book(name = "Math4", 720.3, "Math", 234, Date.from(Instant.now())),
+            Book(name = "Math5", 220.3, "Math", 234, Date.from(Instant.now())),
+            Book(name = "Math16", 20.3, "Math", 234, Date.from(Instant.now())),
+            Book(name = "Math7", 70.3, "Math", 234, Date.from(Instant.now())),))
+
+    listOfListOfBooks.flatten().also { it.forEach { println(it) } }
 }
